ac943074a6ac6a79ef3a5ea34937c3d8882c94c8
ali asadi <engineer_asadi@aol.comgit config --global user.name ali>
2020-05-31 11:57:53 +0430

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
+import { Button, Container } from '@material-ui/core';
 import 'typeface-roboto';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -17,13 +17,14 @@ const useStyles = makeStyles({
     boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
     color: 'black',
     height: 98,
-    padding: '0 90px',
-    
+    // padding: '0 90px',
+    width: 98,
     
   },
 });
 
 export default function Hook(props) {
+
   const classes = useStyles();
   return <Button className={classes.root} 
                   onClick={ ()=>
@@ -32,21 +33,6 @@ export default function Hook(props) {
                     </Button>;
 }
 
-// function Square(props) {
-
-//       return (
-        
-//         <Button  variant='outlined'  
-//                 onClick={ ()=>
-//                 props.onClick()
-//                 }>
-//           {props.value}
-//         </Button>
-        
-//       );
-    
-  
-//   }
 
 
 function checkWining(squares) {
@@ -64,81 +50,77 @@ function checkWining(squares) {
   return false;
 }
 
+// function bestMove(squares) {
+  
+//   let bestMove=-Infinity;
+//   let score;
+//   for (let index = 0; index < squares.length; index++) {
+//     const isAvailableSpot = squares[index];
+//     if (isAvailableSpot!==null) {
+//       squares[index]='O';
+//       return squares;
+//     }
+//   }
 
+
+// }
 
   class Board extends React.Component {
 
-    constructor(props){
-      super(props);
-      this.state={
-        squares: Array(9).fill(null),
-        xTurn:true,
-        
-      };
-
-    }
     
-    handleClick(i){
-      const squares = this.state.squares.slice();
-      if (checkWining(squares)||squares[i]) {
-        return;
-      }
-      squares[i]=this.state.xTurn ? 'X':'O';
-      this.setState({squares,
-      xTurn:!this.state.xTurn});
-    }
+    
 
     renderSquare(i) {
-      return( <Hook 
-              value={this.state.squares[i]}
-              onClick={()=>{this.handleClick(i)}
-              } />);
+      return(
+              <Hook 
+                value={this.props.squares[i]}
+                onClick={()=>{this.props.onClick(i)}
+                }
+              />
+            );
     }
+
+    // aiMove() {
+    //       let newSquares=bestMove(this.state.squares);
+    //       const squares = this.state.squares.slice();
+    //       this.setState({squares,newSquares});
+    // }
   
     render() {
  
       
-      const check=checkWining(this.state.squares);
-      let status;
-      const welcomeText = " X O Game developed by ali asadi";
-      
-      if (check) {
-         status =" the winner is "+ check;
-      }else{
-         status = 'Next player: '+(this.state.xTurn ? 'X':'O');
-      }
+      // if (check) {
+      //    status =" the winner is "+ check;
+      // }else{
+      //    status = 'Next player: '+(this.state.xTurn ? 'X':'O');
+      //    if (this.state.xTurn===false) {
+           
+           
+      //    }
+      // }
 
       return (
-        <Grid
-            container direction="column" justify="center" alignItems="center" >
-          <div item className="grid"  >
-              <Paper >
-                <Typography  variant="h3" justify="center" color="initial">{welcomeText}</Typography>
-              </Paper>
-          </div>
+        
           
-          <Grid container direction="row" justify="center" alignItems="center">
+        <Container wrap="nowrap">
+          <Grid container wrap="nowrap" direction="row"  justify="center" alignItems="center">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
           </Grid>
-          <Grid container direction="row" justify="center" alignItems="center">
+          <Grid  container wrap="nowrap" direction="row" justify="center" alignItems="center">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
           </Grid>
-          <Grid container direction="row" justify="center" alignItems="center">
+          <Grid  container wrap="nowrap" direction="row" justify="center" alignItems="center">
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
           </Grid>
-          <div item className="grid"  >
-            <Paper >
-              <Typography  variant="h3" justify="center" color="primary">{status}
-              </Typography>
-            </Paper>
-          </div>
-        </Grid>
+        </Container>  
+          
+        
 
         
         
@@ -149,12 +131,94 @@ function checkWining(squares) {
   }
   
   class Game extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.state={
+        history:[{
+          squares: Array(9).fill(null),
+        }],
+        xTurn: true,
+        stepNumber:0,
+      };
+    }
+
+    jumpTo(step){
+      this.setState({
+        stepNumber: step,
+        xTurn: (step%2)=== 0
+      });
+    }
+    handleClick(i){
+      const history = this.state.history.slice(0,this.state.stepNumber + 1);
+      const current = history[history.length -1];
+      const squares = current.squares.slice();
+      
+      if (checkWining(squares)||squares[i]) {
+        return;
+      }
+      squares[i]=this.state.xTurn ? 'X':'O';
+      this.setState({
+        history : history.concat([{
+          squares : squares,
+        }]),
+        xTurn: !this.state.xTurn,
+        stepNumber:history.length,
+      });
+    }
+
+
     render() {
+      const welcomeText = " X O Game developed by ali asadi";
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = checkWining(current.squares);
+      let status;
+      if(winner){
+        status = 'winner is :'+winner
+      }else{
+        status = 'next player is : '+(this.state.xTurn ? 'X' : 'O');
+      }
+
+      const moves = history.map((step, move)=> {
+        const desc = move ?
+          'Go to move #' + move :
+          'Reset the game';
+        return(
+          <li key={move}>
+              <Hook 
+                value={desc}
+                onClick={()=>this.jumpTo(move)
+                }
+              />
+          </li>
+        );
+      });
       return (
         <div className="css-selector">
-          <Grid container spacing={1} alignItems="center" >
-            <Board />
+          {/* <Grid container spacing={1} alignItems="center" > */}
+          <Grid
+            container  direction="column" justify="center" alignItems="center" >
+            <div item className="grid"  >
+                <Paper >
+                  <Typography  variant="h3" justify="center" color="initial">{welcomeText}</Typography>
+                </Paper>
+            </div>
+            <Board 
+              squares={current.squares}
+              onClick={(i) =>this.handleClick(i)}
+              />
+          
+            <div item className="grid"  >
+              <Paper >
+                <Typography  variant="h3" justify="center" color="primary">{status}</Typography>
+              </Paper>
+            </div>
+            <div item className="grid"  >
+              {moves}
+            </div>
           </Grid>
+
         </div>
       );
     }
